71c60248b0930637933682b6c1bb717db4c162a1
Sambhav Jain <136801346+DarkLord017@users.noreply.github.com>
2025-04-02 17:10:32 +0530

    Create nice.py

---
+++ hello/nice.py
@@ -0,0 +1,16 @@
+
+from eth_utils import keccak
+
+# Field types
+types = ['address', 'address']
+arguments = f'{",".join(types)}'
+
+# Function definition
+function_definition = f'getPair({arguments})'
+
+# Function signature
+function_signature = '0x' + keccak(text=function_definition).hex()[:8]
+
+function_signature
+
+print(function_signature)
